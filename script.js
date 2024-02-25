1702f7235fc654ceaee0855a23a5cc1acf568f07
Helbri <65601504+Helbri@users.noreply.github.com>
2024-02-25 23:27:58 +0100

    1er push styles + push script et index -step 33

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//déclaration de variables pour les différentes statistiques du jeu
+
 // déclaration par "let" initialisation de la variable "xp" avec une valeur numérique
 let xp = 0;
 let health = 100;
@@ -5,10 +7,22 @@ let gold = 50;
 let currentWeapon = 0;
 let fighting;
 let monsterHealth;
-//déclaration d'un tableau avec plusieurs valeurs -> let variable = [ "un", "deux", "trois" ];
+//déclaration d'une variable avec un tableau avec plusieurs valeurs -> let variable = [ "un", "deux", "trois" ];
+//déclaration d'une variable avec un tableau à une valeur.
 let inventory = ["stick"];
+
+//déclaration de variables "button"
+
 //const est le mot d'une bonne pratique pour signifier que la valeur de cette variable ne changera pas.
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
-// step 22
+
+//déclaration de variables "xpText", "healthText", "goldText", "monsterStats", "monsterName", "text"
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const text = document.querySelector("#text");
+// step 33
