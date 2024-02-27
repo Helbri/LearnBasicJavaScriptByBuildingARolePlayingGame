40583e8a35239ecd100139151448ff702b13fad8
Helbri <65601504+Helbri@users.noreply.github.com>
2024-02-27 18:15:09 +0100

    push styles, push script et index -step 49

--- script.js
+++ script.js
@@ -25,4 +25,46 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const text = document.querySelector("#text");
-// step 33
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+//création de fonctions
+//initialisation des boutons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goTown() {
+    button1.innerText = "Go to store";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    // \" le backslash permet d'échapper les guillemets et d'autres symboles spéciaux
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
+
+function goStore() {
+//innerText permet de changer, ici, le texte, de l'élément qui est sélectionné.
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    text.innerText = "You enter the store."
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+}
+
+function goCave() {
+    console.log("Going to cave.");
+}
+
+function fightDragon (){
+    console.log("Fighting dragon.");
+}
+
+function buyHealth (){};
+
+function buyWeapon (){};
+
+// step 49
